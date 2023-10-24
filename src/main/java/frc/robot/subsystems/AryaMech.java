0cece0b90eb8367c07f58884e7bfc30e365db8ba
Rishay Jain <rishayjain@Rishays-MacBook-Air.local>
2023-10-24 00:14:04 -0700

    added arya sound code

--- src/main/java/frc/robot/subsystems/AryaMech.java
+++ src/main/java/frc/robot/subsystems/AryaMech.java
@@ -4,6 +4,9 @@ import javax.swing.text.StyledEditorKit.BoldAction;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class AryaMech {
     private Timer timer;
@@ -17,6 +20,12 @@ public class AryaMech {
 
     private boolean out;
     
+    NetworkTableInstance inst;
+    NetworkTable table;
+    DoublePublisher audioPub;
+    private Timer audio_timer;
+    private final double time_to_wait_before_turning_off_audio_signal;
+
     public AryaMech(Solenoid shortSolenoid, Solenoid longSolenoid){
         this.shortSolenoid = shortSolenoid;
         this.longSolenoid = longSolenoid;
@@ -25,6 +34,13 @@ public class AryaMech {
         shortIn = 40;
         longOut = shortIn + 5;
         longIn = longOut + 15;
+
+        inst = NetworkTableInstance.getDefault();
+        table = inst.getTable("audio");
+        audioPub = table.getDoubleTopic("trigger_audio").publish();
+        audio_timer = new Timer();
+        time_to_wait_before_turning_off_audio_signal = 1.0;
+
 
         timer= new Timer();
         timer.start();
@@ -39,12 +55,24 @@ public class AryaMech {
             shortSolenoid.set(out);
             out = !out;
         }
+
+        if(audio_timer.hasElapsed(time_to_wait_before_turning_off_audio_signal)){
+            audioPub.set(0.0); // Set the value on networktables to zero so we don't restart the audio
+            audio_timer.stop();
+            audio_timer.reset();
+        }
+
         //pumps end on an in
         else if (timer.hasElapsed(shortIn)){
             shortSolenoid.set(false);
         }
         //tnt explodes, minecraft tnt sound plays
         else if (timer.hasElapsed(longOut)){
+            
+            audioPub.set(1.0); // this publishes the trigger signal to networktables, which gets picked up by the driverstation python script
+            audio_timer.reset(); // setting a timer so that we can stop sending the signal after an appropriate amount of time
+            audio_timer.start(); // if we don't stop sending the trigger signal, the audio might start playing again
+            
             longSolenoid.set(true);
         }
         //reset
