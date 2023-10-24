ce40dd782d7a46a1f58ecca1575e913bf73cb5b2
Rishay Jain <rishayjain@Rishays-MacBook-Air.local>
2023-10-24 00:14:04 -0700

    added arya sound code

---
+++ src/main/deploy/driverstation_script.py
@@ -0,0 +1,36 @@
+# Kevin will strangle me for putting this file here-- I know. 
+# Deal with and save it to the DS computer to be run whenever the HH is on. 
+
+# BEFORE RUNNING THIS:
+# run 'python3 -m pip install pynetworktables' to install the module needed for networktables comms
+
+import time
+from networktables import NetworkTables # <--- this will raise an error when in the java env, create a python venv on DS and run this there
+import pygame
+from numpy import random
+
+NetworkTables.initialize(server='10.1.92.255') # I forgot, what are robot IPs again?
+
+audio_table = NetworkTables.getTable("audio")
+
+pygame.mixer.init()
+sound1 = pygame.mixer.Sound('explode1.mp3') # in the process of pestering arya for audio files
+sound2 = pygame.mixer.Sound('explode2.mp3') # in the process of pestering arya for audio files
+sound3 = pygame.mixer.Sound('explode3.mp3') # in the process of pestering arya for audio files
+sound4 = pygame.mixer.Sound('explode4.mp3') # in the process of pestering arya for audio files 
+the_voicemail = pygame.mixer.Sound('voicemail.mp3') # in case arya doesn't send audio files, use blackmail audio
+
+sounds = [sound1, sound2, sound3, sound4]
+
+while True:
+    trigger_value = audio_table.getNumber("trigger_audio", 0.0)
+    print("trigger:", trigger_value)
+    
+    if trigger_value == 1.0:
+        sound_num = random(0,4)
+        sounds[sound_num].play()
+        
+
+        
+    
+    
--- src/main/java/frc/robot/subsystems/AryaMech.java
+++ src/main/java/frc/robot/subsystems/AryaMech.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class AryaMech {
     private Timer timer;
@@ -13,6 +16,12 @@ public class AryaMech {
     private double shortOut;
     private double shortIn;
     
+    NetworkTableInstance inst;
+    NetworkTable table;
+    DoublePublisher audioPub;
+    private Timer audio_timer;
+    private final double time_to_wait_before_turning_off_audio_signal;
+
     public AryaMech(Solenoid shortSolenoid, Solenoid longSolenoid){
         this.shortSolenoid = shortSolenoid;
         this.longSolenoid = longSolenoid;
@@ -23,12 +32,26 @@ public class AryaMech {
         longOut = shortIn + 5;
         longIn = longOut + 15;
 
+        inst = NetworkTableInstance.getDefault();
+        table = inst.getTable("audio");
+        audioPub = table.getDoubleTopic("trigger_audio").publish();
+        audio_timer = new Timer();
+        time_to_wait_before_turning_off_audio_signal = 1.0;
+
+
         timer= new Timer();
         timer.start();
 
     }
 
     public void periodic() {
+
+        if(audio_timer.hasElapsed(time_to_wait_before_turning_off_audio_signal)){
+            audioPub.set(0.0); // Set the value on networktables to zero so we don't restart the audio
+            audio_timer.stop();
+            audio_timer.reset();
+        }
+
         if(timer.hasElapsed(shortOut)){
             shortSolenoid.set(true);
         }
@@ -36,6 +59,11 @@ public class AryaMech {
             shortSolenoid.set(false);
         }
         else if (timer.hasElapsed(longOut)){
+            
+            audioPub.set(1.0); // this publishes the trigger signal to networktables, which gets picked up by the driverstation python script
+            audio_timer.reset(); // setting a timer so that we can stop sending the signal after an appropriate amount of time
+            audio_timer.start(); // if we don't stop sending the trigger signal, the audio might start playing again
+            
             longSolenoid.set(true);
         }
         else if (timer.advanceIfElapsed(longIn)){
