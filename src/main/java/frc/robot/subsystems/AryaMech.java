0ea8e063b393d985c0b89235f1b097c9187e2d6f
Rishay Jain <rishayjain@Rishays-MacBook-Air.local>
2023-10-24 16:42:41 -0700

    resolving severe incompentencies

--- src/main/java/frc/robot/subsystems/AryaMech.java
+++ src/main/java/frc/robot/subsystems/AryaMech.java
@@ -4,11 +4,12 @@ import javax.swing.text.StyledEditorKit.BoldAction;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class AryaMech {
+public class AryaMech extends SubsystemBase{
     private Timer timer;
     private Solenoid shortSolenoid;
     private Solenoid longSolenoid;
@@ -27,8 +28,8 @@ public class AryaMech {
     private final double time_to_wait_before_turning_off_audio_signal;
 
     public AryaMech(Solenoid shortSolenoid, Solenoid longSolenoid){
-        this.shortSolenoid = shortSolenoid;
-        this.longSolenoid = longSolenoid;
+        // this.shortSolenoid = shortSolenoid;
+        // this.longSolenoid = longSolenoid;
 
         //ik this looks sus but its for ease of change
         shortIn = 40;
@@ -56,6 +57,7 @@ public class AryaMech {
             out = !out;
         }
 
+        audioPub.set(1.0); // this publishes the trigger signal to networktables, which gets picked up by the driverstation python script
         if(audio_timer.hasElapsed(time_to_wait_before_turning_off_audio_signal)){
             audioPub.set(0.0); // Set the value on networktables to zero so we don't restart the audio
             audio_timer.stop();
@@ -68,7 +70,7 @@ public class AryaMech {
         }
         //tnt explodes, minecraft tnt sound plays
         else if (timer.hasElapsed(longOut)){
-            
+            System.out.println("SETTING TRIGGER");
             audioPub.set(1.0); // this publishes the trigger signal to networktables, which gets picked up by the driverstation python script
             audio_timer.reset(); // setting a timer so that we can stop sending the signal after an appropriate amount of time
             audio_timer.start(); // if we don't stop sending the trigger signal, the audio might start playing again
