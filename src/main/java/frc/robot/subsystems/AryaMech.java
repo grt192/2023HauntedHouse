738a34c8f7e9b1bff4b89ee87e045d456ac1b96c
CrolineCrois <maoninii@gmail.com>
2023-10-26 15:13:51 -0700

    skeleton/solenoid to shake before tnt explodes

--- src/main/java/frc/robot/subsystems/AryaMech.java
+++ src/main/java/frc/robot/subsystems/AryaMech.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -12,32 +14,40 @@ public class AryaMech {
     private double longIn;
     private double shortOut;
     private double shortIn;
+
+    private boolean out;
     
     public AryaMech(Solenoid shortSolenoid, Solenoid longSolenoid){
         this.shortSolenoid = shortSolenoid;
         this.longSolenoid = longSolenoid;
 
         //ik this looks sus but its for ease of change
-        shortOut = 0;
-        shortIn = shortOut + 40;
+        shortIn = 40;
         longOut = shortIn + 5;
         longIn = longOut + 15;
 
         timer= new Timer();
         timer.start();
 
+        out = true;
+
     }
 
     public void periodic() {
-        if(timer.hasElapsed(shortOut)){
-            shortSolenoid.set(true);
+        //skeleton pumps thing to set off tnt
+        if(timer.get() < 40 && timer.get() % 2 == 0){
+            shortSolenoid.set(out);
+            out = !out;
         }
+        //pumps end on an in
         else if (timer.hasElapsed(shortIn)){
             shortSolenoid.set(false);
         }
+        //tnt explodes, minecraft tnt sound plays
         else if (timer.hasElapsed(longOut)){
             longSolenoid.set(true);
         }
+        //reset
         else if (timer.advanceIfElapsed(longIn)){
             longSolenoid.set(false);
         }
